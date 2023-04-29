272bbaf6647db324085fd6a83ddbe450e62897da
Elmer Dema <elmerdema2022@gmail.com>
2023-04-29 18:44:55 +0200

    final

--- connect_four.py
+++ connect_four.py
@@ -1,3 +1,9 @@
+'''
+Connect 4 
+Solution to: Programming 2, task 1
+Author:Elmer Dema
+'''
+
 rows = 6
 columns = 7
 board = [[0 for i in range(columns)] for j in range(rows)]
@@ -5,6 +11,9 @@ PLAYER1 = 1
 PLAYER2 = 2
 game_on= True
 player_turn = 1
+
+print("WELCOME TO CONNECT FOUR")
+print("If you want to restart type k")
 
 #checks whether the row is empty(x=0), if it is, it places the sign in the row
 def move(board,move,sign):
@@ -15,10 +24,13 @@ def move(board,move,sign):
             break
     return board
 
+
 def board_style(board):
+      
     for i in board:
         print(f"| {i} |")
-        print("----------------------------")
+        print("----------------------------")    
+
 
 def check_win_row(board, row_placement, column_placement):
     start = board[row_placement][column_placement]
@@ -52,16 +64,21 @@ def check_win_row(board, row_placement, column_placement):
         else:
             break
     return False
+
+
 def check_win_column(board,row_placement,column_placement):
-    start=board[row_placement][column_placement]
-    count=1
-    for i in range(4):
-        if row_placement+i+1<rows:
-            if start==board[row_placement+i+1]:
-                count+=1
+    start = board[row_placement][column_placement]
+    count = 1
+    for i in range(rows):
+        #checks if the row is in the board
+        if i != row_placement and board[i][column_placement] == start:
+            count += 1
+            if count == 4:
+                return True
         else:
-            return False
-    return True
+            count = 1
+    return False
+
 
 def check_win_diagonal(board,row_placement,column_placement):
     start=board[row_placement][column_placement]
@@ -114,8 +131,6 @@ def check_win_diagonal(board,row_placement,column_placement):
         else:
             break
     #right and down   
-    """ if count==4:
-        return True  """   
     for i in range(4-count):
         row=row_placement+i+1
         col=column_placement+i+1
@@ -133,6 +148,11 @@ def check_win_diagonal(board,row_placement,column_placement):
     return False
 
 
+def check_draw(board):
+    for row in board:
+        if 0 in row:
+            return False
+    return True
 
 #If one of the functions returns true, the game ends
 def check_win(board,move):
@@ -143,16 +163,28 @@ def check_win(board,move):
             break
     row_placement=rows-i
     column_placement=move
-    return check_win_column(board,row_placement,column_placement) or check_win_row(board,row_placement,column_placement) or check_win_diagonal(board,row_placement,column_placement)
+    return check_win_column(board,row_placement,column_placement) or check_win_row(board,row_placement,column_placement) or check_win_diagonal(board,row_placement,column_placement) or check_draw(board)
 
-#checks whether the player has won
+def winner(player_turn):
+    print("Player "+str(3 - player_turn)+" won the game")
 
 while game_on == True:
-    user_input = int(input("Player "+str(player_turn)+" enter a move: "))
-    if user_input > -1 and user_input < 6:    
-        if user_input == -1:
+    #asks the player to enter a move (digit)
+    user_input = input("Player "+str(player_turn)+" enter a move: ")
+    if user_input.isdigit() and int(user_input) > -1 and int(user_input) < 7:    
+        move_num = int(user_input)
+        if move_num == -1:
             break
-        board = move(board, user_input, player_turn)
+        board = move(board, move_num, player_turn)
         board_style(board)
         player_turn = 3 - player_turn
-        game_on= not check_win(board, user_input)
+        game_on = not check_win(board, move_num)
+        if not game_on:
+            winner(player_turn)
+        #asks the player whether they want to restart
+    elif user_input.lower() == "k":
+        print("Game has been restarted")
+        board = [[0 for i in range(columns)] for j in range(rows)]
+        board_style(board)
+        player_turn = 1
+        game_on = True
